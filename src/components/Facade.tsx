667158c59f0ad984b0c519dc4c4ffc8eb76501a4
Federico Pellegrini <fellegrini@gmail.com>
2024-05-07 02:12:52 -0300

    mobile styles correction (UI)

--- src/components/Facade.tsx
+++ src/components/Facade.tsx
@@ -6,11 +6,26 @@ import '../styles/facade.css';
 
 function Facade({ isShown = true }) {
   useGSAP(() => {
-    gsap.to('.facade', {
-      x: !isShown ? 0 : -2500,
-      ease: 'power1.inOut',
-      duration: 0.6,
-    });
+    const matchMedia = gsap.matchMedia();
+    matchMedia.add(
+      {
+        isMobile: '(max-width: 475px)',
+        isTablet: '(max-width: 840x)',
+        isDesktop: '(min-width: 841px)',
+      },
+      (context) => {
+        const { conditions } = context;
+        if (conditions) {
+          const { isMobile, isDesktop } = conditions;
+          gsap.to('.facade', {
+            x: !isShown ? 0 : isDesktop ? -2500 : 0,
+            y: !isShown ? 0 : isMobile ? -1200 : 0,
+            ease: 'power1.inOut',
+            duration: 0.6,
+          });
+        }
+      },
+    );
   }, [isShown]);
   return (
     <section className='facade'>
